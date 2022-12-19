c2b1f0aac9ed1cf74068b8b69f3b0790c16ddf6a
Srinivas <kolipakasrinivas456@gmail.com>
2022-12-19 07:17:09 +0530

    I complated monster roldex full project but frogot to push final commit to github

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.css'
 import CardList from './components/card-list/Card-list.componet';
 import SearchBox from './components/search-box/search-box.component';
 
@@ -21,6 +22,9 @@ class App extends React.Component{
         .then(users =>this.setState({Monsters:users}))
     }
 
+    handelchange=(e)=>this.setState({searchFiled:e.target.value})
+
+  
 
 
 
@@ -32,21 +36,17 @@ class App extends React.Component{
         const { Monsters, searchFiled}=this.state
 
         const filterMonsters = Monsters.filter(
-
             Monsters=>Monsters.name.toLowerCase().includes(searchFiled.toLocaleLowerCase())
-            
-            
             )
 
 
 
 
         return(
-            <div>
+            <div className='App'>
+                <h1>Monsters Rolodex</h1>
                 <SearchBox  
-                    handelChange={(e)=>{
-                    this.setState({searchFiled:e.target.value})
-                }}
+                    handelChange={this.handelchange}
                 />
                  <CardList Monsters={filterMonsters}/>
             </div>
